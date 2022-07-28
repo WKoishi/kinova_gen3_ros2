886db7a0b4a54b894d5953c363afa98f0ef1e44a
WKoishi <intelwzt@outlook.com>
2022-07-28 20:30:27 +0800

    Update automatic script

--- src/ros2_srv_template_add.py
+++ src/ros2_srv_template_add.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     MSG_OR_SRV = "srv"
 
     ros2_srv_dir = "src/kortex_driver/srv"
-    target_directory = "src/kortex_driver/include/kortex_driver/generated/interfaces"
+    target_directory = "src/kortex_driver/src"
 
     for root, dirs, files in os.walk(ros2_srv_dir):
         for file in files:
@@ -28,36 +28,77 @@ if __name__ == "__main__":
 
     file_change_flag = False
 
+    # for root, dirs, files in os.walk(target_directory):
+    #     for file in files:
+    #         file_path = os.path.join(root, file)
+    #         file_path_struct = file_path.split(PATH_SPLIT)
+    #         file_name, file_type = os.path.splitext(file_path)
+    #         if file_type == ".h":
+    #             with open(file_path, 'r+', encoding='utf-8') as file_obj:
+    #                 f_contents = file_obj.read()
+    #                 f_data = f_contents.split('\n')
+    #                 for line in f_data:
+    #                     if line:
+    #                         line_temp = line.strip('\t')
+    #                         line_temp = line_temp.strip(' ')
+    #                         line_temp = line_temp.strip(';')
+
+    #                         line_struct = line_temp.split(' ')
+    #                         if line_struct[0] == "ros::ServiceServer" and len(line_struct) == 2:
+    #                             template_detect = line_struct[1].replace("m_service", '')
+    #                             _, _, template_detect = msg_name_format_check(template_detect)
+
+    #                             if template_detect in ros2_msg_srv_list[MSG_OR_SRV]:
+    #                                 file_change_flag = True
+    #                                 template_add = "rclcpp::Service<kortex_driver::" + MSG_OR_SRV + "::" + template_detect + ">::SharedPtr"
+
+    #                                 new_line = line.replace(line_struct[0], template_add)
+    #                                 f_contents = f_contents.replace(line, new_line)
+    #                                 print(line_struct[0], "->", template_add, file_path_struct[-1])
+
+    #                             else:
+    #                                 print("[WARNING]", file_path, template_detect, "not found!")
+
+    #                 if file_change_flag:
+    #                     file_change_flag = False
+    #                     file_obj.seek(0, 0)
+    #                     file_obj.truncate()
+    #                     ret = file_obj.write(f_contents)
+    #                     if ret != len(f_contents):
+    #                         print("[WRITE ERROR]", file_path)
+    #                         exit("[WRITE ERROR]", file_path)
+
+    #             sleep(0.02)
+
     for root, dirs, files in os.walk(target_directory):
         for file in files:
             file_path = os.path.join(root, file)
             file_path_struct = file_path.split(PATH_SPLIT)
             file_name, file_type = os.path.splitext(file_path)
-            if file_type == ".h":
+            if file_type == ".cpp" and "services" in file_path_struct[-1]:
                 with open(file_path, 'r+', encoding='utf-8') as file_obj:
                     f_contents = file_obj.read()
                     f_data = f_contents.split('\n')
                     for line in f_data:
                         if line:
-                            line_temp = line.strip('\t')
-                            line_temp = line_temp.strip(' ')
-                            line_temp = line_temp.strip(';')
+                            line_struct = line.split('=')
+                            if len(line_struct) == 2:
+                                if "m_service" in line_struct[0] and "->create_service(" in line_struct[1]:
+                                    template_detect = line_struct[0].replace("m_service", '')
+                                    template_detect = template_detect.strip('\t')
+                                    template_detect = template_detect.strip(' ')
+                                    _, _, template_detect = msg_name_format_check(template_detect)
 
-                            line_struct = line_temp.split(' ')
-                            if line_struct[0] == "ros::ServiceServer" and len(line_struct) == 2:
-                                template_detect = line_struct[1].replace("m_service", '')
-                                _, _, template_detect = msg_name_format_check(template_detect)
+                                    if template_detect in ros2_msg_srv_list[MSG_OR_SRV]:
+                                        file_change_flag = True
+                                        template_add = "create_service<kortex_driver::" + MSG_OR_SRV + "::" + template_detect + ">"
 
-                                if template_detect in ros2_msg_srv_list[MSG_OR_SRV]:
-                                    file_change_flag = True
-                                    template_add = "rclcpp::Service<kortex_driver::" + MSG_OR_SRV + "::" + template_detect + ">::SharedPtr"
+                                        new_line = line.replace("create_service", template_add)
+                                        f_contents = f_contents.replace(line, new_line)
+                                        print(template_add, file_path_struct[-1])
 
-                                    new_line = line.replace(line_struct[0], template_add)
-                                    f_contents = f_contents.replace(line, new_line)
-                                    print(line_struct[0], "->", template_add, file_path_struct[-1])
-
-                                else:
-                                    print("[WARNING]", file_path, template_detect, "not found!")
+                                    else:
+                                        print("[WARNING]", file_path, template_detect, "not found!")
 
                     if file_change_flag:
                         file_change_flag = False
@@ -69,6 +110,7 @@ if __name__ == "__main__":
                             exit("[WRITE ERROR]", file_path)
 
                 sleep(0.02)
+
 
 
 
